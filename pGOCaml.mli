c14b0fe1e1a9f3966cff714cbb3f40ef0d56e515
rwmjones <rjones@redhat.com>
2008-03-04 10:14:24 +0000

    Added LGPLv2+ copyright notice to files.

--- pGOCaml.mli
+++ pGOCaml.mli
@@ -1,5 +1,23 @@
 (* PG'OCaml is a set of OCaml bindings for the PostgreSQL database.
  * $Id: pGOCaml.mli,v 1.14 2007-10-14 14:52:27 rich Exp $
+ *
+ * PG'OCaml - type safe interface to PostgreSQL.
+ * Copyright (C) 2005-2008 Richard Jones and other authors.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  *)
 
 type 'a t				(** Database handle. *)
