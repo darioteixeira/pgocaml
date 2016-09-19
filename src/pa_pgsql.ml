55b6c984644bbe8b16f6f71f94971b50ee1172bf
Mykola Stryebkov <nick@mykola.org>
2016-09-19 16:31:50 +0300

    unravel type for results too

--- src/pa_pgsql.ml
+++ src/pa_pgsql.ml
@@ -96,10 +96,10 @@ let name_of_type_wrapper ?modifier dbh oid =
  * functions recurses through the pg_type table to see if it happens to be an alias
  * for a type which we do know how to handle.
 *)
-let unravel_type dbh orig_type =
+let unravel_type ?modifier dbh orig_type =
   let rec unravel_type_aux ft =
     try
-      name_of_type_wrapper dbh ft
+      name_of_type_wrapper ?modifier dbh ft
     with PGOCaml.Error msg as exc ->
       let params = [ Some (PGOCaml.string_of_oid ft) ] in
       let rows = PGOCaml.execute dbh ~name:unravel_name ~params () in
@@ -349,7 +349,7 @@ let pgsql_expand ?(flags = []) _loc dbh query =
 	  fun i result ->
 	    let field_type = result.PGOCaml.field_type in
 	    let modifier = result.PGOCaml.modifier in
-	    let fn = name_of_type_wrapper ~modifier my_dbh field_type in
+	    let fn = unravel_type ~modifier my_dbh field_type in
 	    let fn = fn ^ "_of_string" in
 	    let nullable =
 	      f_nullable_results ||
