darioteixeira/pgocaml

commit 7fe11fc0692000a58d4b244156b5b06903df699a

fix string escaping

src/PGOCaml_generic.ml
@@ -1569,35 +1569,13 @@ let option_map f = function
   | Some x -> Some (f x)
   | None -> None
 
-let escape_string str =
-  let buf = Buffer.create 128 in
-  for i = 0 to String.length str - 1 do
-    match str.[i] with
-      | '"' | '\\' as x -> Buffer.add_char buf '\\'; Buffer.add_char buf x
-      | x -> Buffer.add_char buf x
-  done;
-  Buffer.contents buf
-
 let string_of_bool_array a = string_of_any_array (List.map (option_map string_of_bool) a)
 let string_of_int32_array a = string_of_any_array (List.map (option_map Int32.to_string) a)
 let string_of_int64_array a = string_of_any_array (List.map (option_map Int64.to_string) a)
-let string_of_string_array a = string_of_any_array (List.map (option_map escape_string) a)
+let string_of_string_array a = string_of_any_array (List.map (option_map String.escaped) a)
 let string_of_float_array a = string_of_any_array (List.map (option_map string_of_float) a)
 
-let string_of_bytea b =
-  let len = String.length b in
-  let buf = Buffer.create (len * 2) in
-  for i = 0 to len - 1 do
-    let c = b.[i] in
-    let cc = Char.code c in
-    if cc < 0x20 || cc > 0x7e then
-      Buffer.add_string buf (sprintf "\\%03o" cc) (* non-print -> \ooo *)
-    else if c = '\\' then
-      Buffer.add_string buf "\\\\" (* \ -> \\ *)
-    else
-      Buffer.add_char buf c
-  done;
-  Buffer.contents buf
+let string_of_bytea bytea = "\\x" ^ Digest.(to_hex (string bytea))
 
 let string_of_string (x : string) = x
 let oid_of_string = Int32.of_string

commit f6c76e080891e83b1d91104b26873bfc49cd3405

fix string encoding
fixes bug introduced in commit 7fe11fc0692000a58d4b244156b5b06903df699a

src/PGOCaml_generic.ml
@@ -1585,7 +1585,21 @@ let string_of_int64_array a = string_of_any_array (List.map (option_map Int64.to
 let string_of_string_array a = string_of_any_array (List.map (option_map String.escaped) a)
 let string_of_float_array a = string_of_any_array (List.map (option_map string_of_float) a)
 
-let string_of_bytea bytea = "\\x" ^ Digest.(to_hex (string bytea))
+let string_of_bytea =
+  let char_hex n = (* copy of Digest.char_hex *)
+    Char.unsafe_chr (n + if n < 10 then Char.code '0' else (Char.code 'a' - 10))
+  in
+  let to_hex d = (* copy of Digest.to_hex without the 16 character limitation *)
+    let n = String.length d in
+    let result = Bytes.create (2*n) in
+    for i = 0 to n-1 do
+      let x = Char.code d.[i] in
+      Bytes.unsafe_set result (i*2) (char_hex (x lsr 4));
+      Bytes.unsafe_set result (i*2+1) (char_hex (x land 0x0f));
+    done;
+    Bytes.unsafe_to_string result
+  in
+  fun bytea -> "\\x" ^ to_hex bytea
 
 let string_of_bytea_array a =
   string_of_any_array (List.map (option_map string_of_bytea) a)