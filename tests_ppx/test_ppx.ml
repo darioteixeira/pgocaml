c53c49867d3e4de8f25556b1d9b21c3daa3aec14
John Christopher McAlpine <christophermcalpine@gmail.com>
2019-11-07 16:17:46 +0000

    optimize duplicate parameters

--- tests_ppx/test_ppx.ml
+++ tests_ppx/test_ppx.ml
@@ -4,29 +4,30 @@ let init_dbh dbh =
 id serial not null primary key,
 name text not null,
 salary int4 not null,
+pay int4 not null,
 email text
 )"]
 
 let employee_exists dbh ?email n =
-  [%pgsql dbh "SELECT EXISTS (SELECT 1 FROM employees WHERE name = $n AND email = $?email AND email = $email)"]
+  [%pgsql dbh "SELECT EXISTS (SELECT 1 FROM employees WHERE name = $n AND email = $?email)"]
 
 let () =
   let dbh = PGOCaml.connect () in
 
   init_dbh dbh;
 
-  let insert name salary email = [%pgsql dbh "insert into employees (name, salary, email) values ($name, $salary, $?email)"] in
+  let insert name salary  email = [%pgsql dbh "insert into employees (name, salary, pay, email) values ($name, $salary, $salary, $?email)"] in
   insert "Ann" 10_000_l None;
   insert "Bob" 45_000_l None;
   insert "Jim" 20_000_l None;
   insert "Mary" 30_000_l (Some "mary@example.com");
 
-  let rows = [%pgsql dbh "select id, name, salary, email from employees"] in
+  let rows = [%pgsql dbh "select id, name, salary, pay, email from employees"] in
   List.iter
     begin
-      fun (id, name, salary, email) ->
+      fun (id, name, salary, pay, email) ->
         let email = match email with Some email -> email | None -> "-" in
-        Printf.printf "%ld %S %ld %S\n" id name salary email
+        Printf.printf "%ld %S %ld %ld %S\n" id name salary pay email
     end rows;
 
   let ids = [ 1_l; 3_l ] in
